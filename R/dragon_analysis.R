f89d77a3efc6c1adb0be8fb25e58572543cacc9c
kingsleyshaklebolt <me@audhalbritter.com>
2020-12-02 19:40:52 +0100

    add mod 2

--- R/dragon_analysis.R
+++ R/dragon_analysis.R
@@ -8,7 +8,7 @@ source("R/import_dragon_data.R")
 
 # Run model
 mod <- lm(body_size ~ body_length + var1, data = dragon)
-mod2
+mod2 <- lm(body_sizen ~ body_length * var1, data = dragon) 
 tidy(mod)
 
 newmod
